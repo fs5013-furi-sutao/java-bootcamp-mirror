45d27cc10ae8b307f4c4e39f0fee0ea3dbed0f7b
fs5013-furi-sutao <takada.riuji@gmail.com>
2023-10-24 12:38:09 +0900

    初級PG認定試験 単体テスト教材の追加

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
         description: 'およそ 250 時間で Java 言語とプログラミングの 基礎 ～ 発展的内容までの知識と技能を手に入れるフリースタイル ITS 所属の方向けのプログラムです',
         baseDirectory: path.resolve(__dirname, '../'),
         author: 'Hashimoto Natsuki',
-        sections: ['Java Bootcamp', '01.イントロ', '02.入門編', '03.初級編', '04.中級編', '05.上級編', '06.マスタ編', 'サイトについて'],
+        sections: ['Java Bootcamp', '01.イントロ', '02.入門編', '03.初級編', '04.中級編', '05.上級編', '06.マスタ編' , '07.初級PG認定試験対策' , 'サイトについて'],
         navItems: [{ title: 'ホーム', url: '/home/' }],
         // twitterAccount: 'neoziro',
         githubRepositoryURL: 'https://github.com/fs5013-furi-sutao/java-bootcamp',
