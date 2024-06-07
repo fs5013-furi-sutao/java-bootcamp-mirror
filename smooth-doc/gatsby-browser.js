d3a7e16f891652942756109ebd59c74ce1690139
Riuji Takada <h_takada@freestyles.jp>
2024-06-07 12:06:25 +0900

    Enumに関する補足を追加

--- smooth-doc/gatsby-browser.js
+++ smooth-doc/gatsby-browser.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import { RootWrapper } from './src/components/RootWrapper'
 import { PageWrapper } from './src/components/PageWrapper'
+import ReactDOM from "react-dom/client";
 import Prism from 'prism-react-renderer/prism';
+
+export const replaceHydrateFunction = () => {
+  return (element, container) => {
+    const root = ReactDOM.createRoot(container)
+    root.render(element)
+  }
+}
 
 export const wrapRootElement = ({ element }) => {
   return <RootWrapper>{element}</RootWrapper>
